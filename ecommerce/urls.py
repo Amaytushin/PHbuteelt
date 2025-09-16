ce49903679e804804225c21be82fa78316647e3b
Amaytushin <atuvshin779@gmail.com>
2025-09-16 15:41:40 +0800

    lab2 done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('', views.index, name='index'),
+#    path('', views.index, name='index'),
    path('', include('store_app.urls')),
 
 
--- store_app/views.py
+++ store_app/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
-from .form import Form
 
-# Create your views here.
-def baraa (request):
-    form = Form()
-    return render (request, 'baraa.html',{"form": form})
+
+def show_baraa_form(request):
+    return render(request, "baraa.html")
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def show_cart(request):
+    return render(request, "cart.html")
+
+
+def show_dashboard(request):
+    return render(request, "dashboard.html")
+
+
+def show_order_complete(request):
+    return render(request, "order_complete.html")
+
+
+def show_place_order(request):
+    return render(request, "place-order.html")
+
+
+def show_product_detail(request):
+    return render(request, "product-detail.html")
+
+
+def show_register(request):
+    return render(request, "register.html")
+
+
+def show_search(request):
+    return render(request, "search-result.html")
+
+
+def show_signin(request):
+    return render(request, "signin.html")
+
+
+def show_store(request):
+    return render(request, "store.html")
+
+
+def show(request):
+    pass
